8dda044db175811d57421355799015b72754c4d4
Adam <adamgloria08@gmail.com>
2020-04-04 22:00:48 -0500

    first commit

---
+++ Adams Erotica.py
@@ -0,0 +1,130 @@
+
+from time import sleep
+import sys
+
+def typewriter(str):
+    for x in str:
+        print(x, end='')
+        sys.stdout.flush()
+        sleep(0.04)
+    print('')
+
+health=100
+choices=""
+
+typewriter("Welcome to Adam's Erotic Novel Adventure\n")
+
+typewriter("to begin down this sexy journey, we are first met with a man named Kknad.")
+typewriter("He is ready to party in this tavern, However, It has been noted that there are many 'Traps'.... These you must be weary of")
+
+choices=input("Lets begin shall we! Enter Tavern Y/N? ")
+if choices == "N" or choices=="n":
+    typewriter("You go home with a massive erection that never goes away, there fore it explodes and you instantly die of blood loss")
+    health=health-100
+    typewriter(f" You lost 100 Health. Your Total Health is {health}. Game Over Nigga. :(")
+elif choices == "Y" or choices=="y":
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    typewriter("you enter the tavern and notice some fine looking elves at the bar as well as the stripper poles... You ponder where to go..")
+    choices=input("Shall I dance my way to the bar? (Y/N?) ")
+    if choices =="Y" or choices=="y":
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        typewriter("You groove slowly to bar. The Fine looking elf looks at you with a glimmer in her eye 'hey bby.. what could I get you?'")
+
+        typewriter("now is your chance...")
+        print("-----------------------------------------------------------------------------------------------------------")
+        print("option 1: Hey baby, can i have a glemming flow or acid on the rocks, also, can I take a sample of you... maybe take a ride on my horse?\n")
+        print("option 2: Slap your Weiner on the bar and say 'Dinner Time Baby...'\n")
+        print("option 3: So what do they call you around here baby?\n")
+        choices=input("1,2,3? ")
+        if choices=="1":
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            typewriter("You talk smooth sugga muffin, ill have you know that I am a male and im hung like a bull. you might want to step of unless you want to see what real love really is.... ;)\n")
+            health=health-50
+            typewriter(f"ARGGGGH my sex drive!!! Your Peter begins to shrink... Total Health is {health}.\n")
+
+            typewriter("'Listen baby' why don't you talk to my home girl Diamond, shes is that blue elf over there with the fishnet leggins and the thong. Shes willing to give anyone a good time if you say the right things.\n")
+           
+            choices=input("Option 1: Go speak to Diamond? Option 2: go the fuck home before this night gets any worse (1 or 2? ")
+            if choices == "1":
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                typewriter("You slowly walk up to Diamond from behind. You tap on her shoulder.\n")
+                typewriter("She turns to you with devilish looking eyes and long serpantlike tounge.. she is obvioulsy a succubus. this is your chance for maximum pleasure\n")
+                print("-----------------------------------------------------------------------------------------------------------")
+                print("option 1: say 'Hey sexy I heard You are willing to give anyone a good time.. how bout you show me what you got?\n")
+                print("option 2: say 'Umm.... Im kinda in the mood. mind helping a bruthu out by helping me drain the snake? if you know what I mean?\n")
+                print("option 3: say 'You me do the bang bang now or else I will summon your dark overseer and order you to shred you to bits of dog chow for not doing your job\n")
+                print("option 4: say 'I hear you give men like me a good time, how bout you show me what thats all about? I want you to charm me and suck the soul out of me.. take me to the underworld with you. I just need this damn erection to go away..\n")
+               
+                choices=input("option 1,2,3,or 4? ")
+               
+                if choices == "1":
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                    health=health-50
+                    typewriter("The succubus grows large teeth and her eyes begin to go black.. she grabs you with her tail and pulls you in her direction. She yells 'HOWS THIS FOR A GOOD TIME?!!'\n")
+                    typewriter(f"with one giant bite, she rips off your head clean from your body and begins to devour your enternal organs starting from the heart.. its safe to say you made the wrong decision.. Total Health is {health} Game over..\n")
+                elif choices =="2":
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                    health=health-50
+                    typewriter("the succubus smiles,'sure buddy ill help you drain the snake'")
+                    typewriter("As your excitment grows, you notice her tail taking the shape of what appears to be a cathider.")
+                    typewriter("before you have time to run, she grabs you with her long arms and rams the serpintine needle down your peehole and begins to suck the blood out of your erection..\n")
+                    typewriter(f"not only did you loose your erection completely, you have shrunk from having a normal size wang to a thumbtack size wang. Game over for life bro.. Health is not {health}\n")
+                elif choices =="3":
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                    health=health-50
+                    typewriter("FUCK YOU ASSHOLE I WONT LET YOU THREATEN ME!!\n")
+                    typewriter("you tremble in fear and notice the succubus grow to large hammers for arms\n")
+                    typewriter(f"before you could even run, she smashes your head into a pancake with her to hammer arms.. Maybe next time buddy.. your health is now {health} game over dawg\n")
+                elif choices =="4":
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                    typewriter("you sure have a way with words and you definently got what im looking for.. let me show you a good time..\n")
+                    typewriter("the succubus mounts you and pentration is now happening right in front of all the people in the tavern. her eyes are glowing with fire and she is screaming in excitment!\n")
+                    typewriter("boi this is all i was looking for this whole time.. as you getting close to climaxing you notice her flaps grow teeth... the minute you climaxe your soul begins to get sucked into her body.\n")
+                    typewriter("SHIT!! you yell, THIS IS AMAZING OMG BEST CLIMAX EVR!! before you can finish your next sentence, your body shrives up into beef jerky and is sucked into the body of the succubus.. you are now dead..\n")
+                    typewriter("you did have a great time however during the time you were alive.. 10/10 worth it...\n")
+            elif choices =="2":
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                typewriter("You run home before shit gets worse.. you are now forever horny.. Game Over\n")           
+    
+    
+
+
+
+        elif choices == "2":
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            typewriter("ITS A TRAAAP the Sexy bartender wips out her weiner which is the size of a baby's arm. She grows long teeth and a dragon tail and beings to yell 'DINNER TIME BABY!!'\n")
+            typewriter("she grabs you by the back of the neck and forces you to devoure the baby sized shlong.. You are instantly turned off completely and loose all sex drive for the night.. on top of that your manhood has been tarnished permeneantly.\n")
+            health=health-100
+            typewriter(f"You are choking and gasping for air... MMMPHFJDHOPPFHDOF... You pass out from exaustion as a massive load is unleashed down your esophagous... you ded.. Total Health is {health}.\n")
+
+
+        elif choices =="3":
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            typewriter("You talk smooth honey.. but just so you know I am a man.. if you want real love.. go check out the stripper poles. the Darks elves are always looking for handsome chaps like yourself.\n")
+            typewriter("you dodged a bullet there.. time to make my way to the stripper poles\n")
+            typewriter("You groove slowly to poles and are met with 4 fine red elves. you got one shot at this.. this could either end the night in my favor.. or I may be going home..\n")
+            print("-----------------------------------------------------------------------------------------------------------")
+            print("option 1: say, I hear you girls give guys like me a good time howbout we make our way to the back of my horse? maybe we can get to know eachother a bit better?\n")
+            print("option 2: Look im gonna be honest with you elves.. you are all extremely attractive and Im extremely hard. wanna get it on in my carraige? or even on the back of my horse?\n")
+            choices=input("option 1 or 2 ")
+            if choices =="1":
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                typewriter("the elves say together, sounds good sexy... lets go check out your horse\n")
+                typewriter("you lead the ladies to your horse..\n")
+                typewriter("the elves stop in their tracks.. THATS A DONKY NOT A HORSE. you such a looser Knadd.. go find yourself another groupie!!\n")
+                typewriter("you selfesteme has taking a huge blow and you begine to ride your donky home... goodbye tavern...\n")
+                typewriter("Game Over Nigga")
+            elif choices =="2":
+                print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                typewriter("the elves look at eachother and ask talk amongst themselve.. okay babe lets check this carraige out\n")
+                typewriter("the taller elf yells in excitment 'OH MY! what a nice ride you got!' they all hop onto the carriage completely ignoring that my horse is actually a donky.\n")
+                typewriter("'well what are you waiting for Kknad... lets go have us a good night.. ;)' All hyped up in excitment Kknad hops on his donky and takes these fine elves home for a good night..\n")
+                typewriter("at least.. so he thought it would be a good night......\n")
+                typewriter("to be continued..\n")
+    if choices =="N" or choices=="n":
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        typewriter("you pussy out and just talk to the male bartender instead..\n")
+        typewriter("you request 10 shot of moonshine on the rocks and down them all in 5 minutes\n")
+        typewriter("you intstantly die.. Game over..\n")
+
+input()
